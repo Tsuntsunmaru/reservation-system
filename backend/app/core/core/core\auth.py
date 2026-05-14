e7821b06c9dd69d8943c9b1bcca8696baca53929
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-14 09:42:14 +0900

    Create core\auth.py

---
+++ backend/app/core/core/core\auth.py
@@ -0,0 +1,19 @@
+from jose import jwt
+from passlib.context import CryptContext
+
+SECRET KEY = "secret"
+ALGORITHM = "HS256"
+
+pwd context = CryptContext(schemes=["bcrypt"])
+
+def hash_password(p):
+    return pwd_context.hash(p)
+
+def verify_password(p, h):
+    return pwd_context.verify(p, h)
+
+def create_token(data):
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_token(token):
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
