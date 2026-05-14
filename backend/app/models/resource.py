93389a23ce608214fe82186c8b89fbb2801b3bfd
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-14 09:55:37 +0900

    Create resource.py

---
+++ backend/app/models/resource.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Resource(Base):
+    __tablename__ = "resources"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    type = Column(String)
