e8150cd51e8d69b290ae3140d8a97496c4bc7c91
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-14 11:13:25 +0900

    Create main.py

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.database import Base, engine
+
+from app.routers import auth, booking, resource, admin
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+app.include_router(resource.router)
+app.include_router(booking.router)
+app.include_router(admin.router)
