8d2e198707cd45d04084aeb485960389c3998d07
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-18 18:27:36 +0900

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,88 +1,15 @@
-const API = "https://reservation-system-nle7.onrender.com";
-const token = localStorage.getItem("token");
-
-let calendar;
-
-async function loadResources() {
-  const res = await fetch(API + "/resources");
-  const data = await res.json();
-
-  const select = document.getElementById("resourceSelect");
-  select.innerHTML = "";
-
-  data.forEach(r => {
-    const opt = document.createElement("option");
-    opt.value = r.id;
-    opt.textContent = r.name;
-    select.appendChild(opt);
-  });
-}
-
-async function loadBookings(resourceId) {
-  const res = await fetch(API + "/bookings");
-  const data = await res.json();
-
-  return data
-    .filter(b => b.resource_id == resourceId)
-    .map(b => ({
-      id: b.id,
-      title: "予約",
-      start: new Date(b.start_at),
-      end: new Date(b.end_at),
-      color: "#e84118"
-    }));
-}
-
-function generateWeekendEvents() {
-  return {
-    daysOfWeek: [0, 6],
-    display: "background",
-    color: "#dcdde1"
-  };
-}
-
-function initCalendar() {
-
-  const resourceId = document.getElementById("resourceSelect").value;
-
-  loadBookings(resourceId).then(bookings => {
-
-    const calendarEl = document.getElementById("calendar");
-
-    if (calendar) calendar.destroy();
-
-    calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: "timeGridWeek",
-      selectable: true,
-
-      events: [
-        ...bookings,
-        generateWeekendEvents()
-      ],
-
-      select: function(info) {
-        alert("select動いた！");
-      }
-    });
-
-    calendar.render();
-  });
-}
-
-// 初期化
-document.getElementById("resourceSelect")
-  .addEventListener("change", initCalendar);
-
-initCalendar();
 document.addEventListener("DOMContentLoaded", () => {
-window.onload = () => {
-  loadResources();
-  initCalendar();
-}
 
+  const calendarEl = document.getElementById("calendar");
+
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "timeGridWeek",
+    selectable: true,
+
+    select: function(info) {
+      alert("動いた！！！！");
+    }
+  });
+
+  calendar.render();
 });
-
-function logout(){
-  localStorage.removeItem("token");
-  alert("ログアウト");
-}
