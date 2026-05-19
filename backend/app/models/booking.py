7eeceb8c6f8e45697bb0a9bf5ff9e5e204216b1b
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-19 14:19:12 +0900

    Update booking.py

--- backend/app/models/booking.py
+++ backend/app/models/booking.py
@@ -5,6 +5,7 @@ class Booking(Base):
     __tablename__ = "bookings"
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
+    user_name = Column(String)
     resource_id = Column(Integer, ForeignKey("resources.id"))
     start_at = Column(DateTime)
     end_at = Column(DateTime)
