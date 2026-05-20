f8a301b4d2eb8c2f3f8f8025b00c73ee9c928913
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-20 11:53:21 +0900

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,46 +1,67 @@
-console.log("app.js読み込まれた");
-<script>
+const API = "https://reservation-system-nle7.onrender.com";
+
 window.onload = () => {
 
   const calendarEl = document.getElementById("calendar");
 
-  const calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "timeGridWeek",
-    selectable: true,
+  let calendar;
 
-    select: function(info) {
+   async function loadBookings() {
+    const res = await fetch(API + "/bookings");
+    const bookings = await res.json();
 
-      // ✅ 日付整形
-      const start = new Date(info.startStr).toLocaleString("ja-JP");
-      const end = new Date(info.endStr).toLocaleString("ja-JP");
+    return bookings.map(b => ({
+      title: `${b.user_name}`,                 
+      start: b.start_at,
+      end: b.end_at,
+      color: b.resource_id === 1
+        ? "#e84118"    // 車
+        : "#0984e3"    // 会議室
+    }));
+  }catch (e) {
+    console.log("API取得失敗", e);
+    return [];
 
-      // ✅ 見やすい確認メッセージ
-      const message =
-`========================
-      予約確認
-========================
 
-開始：${start}
-終了：${end}
+    async function initCalendar() {
 
-この内容で予約しますか？`;
+    const events = await loadBookings();
 
-      // ✅ confirm
-      const ok = confirm(message);
-      if (!ok) return;
+    if (calendar) calendar.destroy();
 
-      alert("✅ 予約成功");
+    calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: "timeGridWeek",
+      selectable: true,
+      events: events,
 
-      // ✅ カレンダーに表示
-      calendar.addEvent({
-        title: "予約",
-        start: info.start,
-        end: info.end,
-        color: "#e84118"
-      });
-    }
-  });
+      select: async function(info) {
 
-  calendar.render();
+        const ok = confirm("予約しますか？");
+        if (!ok) return;
+
+        const res = await fetch(API + "/bookings", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            resource_id: 1,   // ←あとでselect連動させる
+            start_at: info.startStr,
+            end_at: info.endStr
+          })
+        });
+
+        if (res.ok) {
+          alert("予約成功");
+          initCalendar();
+        } else {
+          alert("予約失敗");
+        }
+      }
+    });
+
+    calendar.render();
+  }
+
+  initCalendar();
 };
-</script>
