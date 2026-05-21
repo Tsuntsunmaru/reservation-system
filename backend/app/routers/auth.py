7b6cfc7913882b2b508d3b99ae70ba2ca4965618
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-21 17:59:19 +0900

    Update auth.py

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -23,6 +23,7 @@ def register(user: UserCreate):
 
     new_user = User(
         email=user.email,
+        username=data.username,
         password=hash_password(user.password),
         role="user"
     )
