88f74dc2637ec26d974da4974eedbc408451abb6
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-21 18:09:35 +0900

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import booking,resource,auth,admin
 
+from app.database import engine
+from sqlalchemy import text
+
 app = FastAPI()
 
 @app.get("/")
@@ -21,3 +24,15 @@ app.include_router(booking.router)
 app.include_router(resource.router)
 app.include_router(auth.router)
 app.include_router(admin.router)
+
+with engine.connect() as conn:
+
+    res = conn.execute(text("""
+        SELECT column_name
+        FROM information_schema.columns
+        WHERE table_name='users' AND column_name='username';
+    """))
+
+    if res.fetchone() is None:
+        conn.execute(text("ALTER TABLE users ADD COLUMN username VARCHAR;"))
+        conn.commit()
