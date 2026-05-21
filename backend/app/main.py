9b8beba034d6bfa03acfc855b790c1a6d22bc73e
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-21 15:13:59 +0900

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers import booking  # ←1つだけ入れる
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "ok"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(booking.router)
