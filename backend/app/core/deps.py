d3d57c1ac655f2b654f5a1698a08bdbcb1c6e211
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-22 14:22:37 +0900

    Create deps.py

---
+++ backend/app/core/deps.py
@@ -0,0 +1,14 @@
+from fastapi import Header
+from app.database import SessionLocal
+from app.models.user import User
+from app.core.auth import decode_token
+
+def get_user(authorization: str = Header(None)):
+    db = SessionLocal()
+
+    if not authorization:
+        return db.query(User).first()
+
+    token = authorization.replace("Bearer ", "")
+    payload = decode_token(token)
+    return db.query(User).get(payload["user_id"])
