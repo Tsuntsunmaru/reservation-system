99851487e9ba8e6d0d06ca490c2471866c908f41
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-22 13:52:43 +0900

    Update user.py

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,1 +1,10 @@
+from pydantic import BaseModel
 
+class UserCreate(BaseModel):
+    email: str
+    password: str
+    username: str
+
+class LoginUser(BaseModel):
+    email: str
+    password: str
