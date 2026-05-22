51798d424a6be48d4fe04c170be1bb8190cdd1fc
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-22 15:14:16 +0900

    Update booking.py

--- backend/app/routers/booking.py
+++ backend/app/routers/booking.py
@@ -12,52 +12,62 @@ router = APIRouter()
 @router.get("/bookings")
 def get_bookings():
     db = SessionLocal()
-    return db.query(Booking).all()
+    try:
+        return db.query(Booking).all()
+    finally:
+        db.close()
 
 
 @router.post("/bookings")
 def create_booking(data: BookingIn, user: User = Depends(get_user)):
     db = SessionLocal()
 
-    # ✅ 入力値を統一（重要）
-    start_at = data.start_at.replace(tzinfo=None)
-    end_at = data.end_at.replace(tzinfo=None)
-
-    ok, msg = can_book(
+    try:
+        start_at = data.start_at.replace(tzinfo=None)
+        end_at = data.end_at.replace(tzinfo=None)
+        
+        ok, msg = can_book(
         db,
         data.resource_id,
         start_at,
         end_at,
         user
-    )
+        )
+        
+        if not ok:
+            raise HTTPException(400, msg)
+            
+        db.add(Booking(
+            user_id=user.id,
+            user_name=user.username,
+            resource_id=data.resource_id,
+            start_at=start_at,
+            end_at=end_at
+        ))
+        
+        db.commit()
+        return {"msg": "ok"}
 
-    if not ok:
-        raise HTTPException(400, msg)
-
-    db.add(Booking(
-        user_id=user.id,
-        user_name=user.username,
-        resource_id=data.resource_id,
-        start_at=start_at,
-        end_at=end_at
-    ))
-
-    db.commit()
-    return {"msg": "ok"}
+    finally:
+        db.close()
     
 @router.delete("/bookings/{booking_id}")
 def delete_booking(booking_id: int ,user: User = Depends(get_user)):
     db = SessionLocal()
 
-    booking = db.query(Booking).filter(Booking.id == booking_id).first()
-
-    if not booking:
-        raise HTTPException(404, "見つからない")
-
-    if booking.user_id != user.id:
-        raise HTTPException(403,"自分の予約のみ削除できます")
-
-    db.delete(booking)
-    db.commit()
-
-    return {"msg": "deleted"}
+    try:
+        booking = db.query(Booking).filter(Booking.id == booking_id).first()
+        
+        if not booking:
+            raise HTTPException(404, "見つからない")
+            
+        if booking.user_id != user.id:
+            raise HTTPException(403,"自分の予約のみ削除できます")
+            
+        db.delete(booking)
+        db.commit()
+        
+        return {"msg": "deleted"}
+        
+    finally:
+        db.close()
