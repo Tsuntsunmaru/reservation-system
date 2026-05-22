6fec491701261f01e4691dc482a48c73dece56c1
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-22 13:33:14 +0900

    Update auth.py

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -44,3 +44,15 @@ def login(user: LoginUser):
         raise HTTPException(status_code=401, detail="Unauthorized")
 
     return {"token": create_token({"user_id": db_user.id})}
+
+@router.get("/fix-user")
+def fix_user():
+    db = SessionLocal()
+
+    user = db.query(User).filter(User.email == "admin3@nttlogisco.com").first()
+
+    if user:
+        user.username = "admin"
+        db.commit()
+
+    return {"msg": "ok"}
