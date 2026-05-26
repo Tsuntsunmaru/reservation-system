cab45c64a6dddf962b1d72cd640f5ecb58c1f800
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-26 18:09:20 +0900

    Update booking.py

--- backend/app/models/booking.py
+++ backend/app/models/booking.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, DateTime, ForeignKey, String
+from sqlalchemy import Column, Integer, DateTime, ForeignKey, String, Text
 from app.database import Base
 
 class Booking(Base):
