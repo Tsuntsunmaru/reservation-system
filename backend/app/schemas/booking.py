23f2d003dfb9a255f4ff027f45603a2fa7133bae
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-26 17:22:33 +0900

    Update booking.py

--- backend/app/schemas/booking.py
+++ backend/app/schemas/booking.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
+from typing import Optional
 from datetime import datetime
 
 class BookingIn(BaseModel):
     resource_id: int
     start_at: datetime
     end_at: datetime
+    title: Optional[str] = None
+    note: Optional[str] = None
