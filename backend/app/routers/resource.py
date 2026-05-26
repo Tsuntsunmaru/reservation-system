1f1d7dfdb01615835b2e4ef511ee6fc79cb3e09c
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-26 15:45:05 +0900

    Update resource.py

--- backend/app/routers/resource.py
+++ backend/app/routers/resource.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
-from app.database import SessionLocal
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.database import get_db
 from app.models.resource import Resource
 
 router = APIRouter()
 
 @router.get("/resources")
-def get_resources():
-    db = SessionLocal()
+def get_resources(db: Session = Depends(get_db)):
     return db.query(Resource).all()
