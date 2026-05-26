37d1e5239d0c5c1f9f9be3200723c1a747118d28
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-26 15:42:07 +0900

    Update booking.py

--- backend/app/routers/booking.py
+++ backend/app/routers/booking.py
@@ -1,42 +1,35 @@
-from fastapi import APIRouter, Depends, HTTPException, Header
-from app.database import SessionLocal
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
 from app.schemas.booking import BookingIn
 from app.services.booking_logic import can_book
 from app.models.booking import Booking
 from app.models.user import User
-from app.core.auth import decode_token
 from app.core.deps import get_user
 
 router = APIRouter()
 
+
 @router.get("/bookings")
-def get_bookings():
-    db = SessionLocal()
-    try:
-        return db.query(Booking).all()
-    finally:
-        db.close()
+def get_bookings(db: Session = Depends(get_db)):
+    return db.query(Booking).all()
 
 
 @router.post("/bookings")
-def create_booking(data: BookingIn, user: User = Depends(get_user)):
-    db = SessionLocal()
-
+def create_booking(
+    data: BookingIn,
+    user: User = Depends(get_user),
+    db: Session = Depends(get_db)
+):
     try:
         start_at = data.start_at.replace(tzinfo=None)
         end_at = data.end_at.replace(tzinfo=None)
-        
-        ok, msg = can_book(
-        db,
-        data.resource_id,
-        start_at,
-        end_at,
-        user
-        )
-        
+
+        ok, msg = can_book(db, data.resource_id, start_at, end_at, user)
+
         if not ok:
             raise HTTPException(400, msg)
-            
+
         db.add(Booking(
             user_id=user.id,
             user_name=user.username,
@@ -44,38 +37,35 @@ def create_booking(data: BookingIn, user: User = Depends(get_user)):
             start_at=start_at,
             end_at=end_at
         ))
-        
+
         db.commit()
         return {"msg": "ok"}
 
-    except Exception as e:
+    except Exception:
         db.rollback()
-        raise e
+        raise
 
-    finally:
-        db.close()
-    
+
 @router.delete("/bookings/{booking_id}")
-def delete_booking(booking_id: int ,user: User = Depends(get_user)):
-    db = SessionLocal()
-
+def delete_booking(
+    booking_id: int,
+    user: User = Depends(get_user),
+    db: Session = Depends(get_db)
+):
     try:
         booking = db.query(Booking).filter(Booking.id == booking_id).first()
-        
+
         if not booking:
             raise HTTPException(404, "見つからない")
-            
+
         if booking.user_id != user.id:
-            raise HTTPException(403,"自分の予約のみ削除できます")
-            
+            raise HTTPException(403, "自分の予約のみ削除できます")
+
         db.delete(booking)
         db.commit()
-        
+
         return {"msg": "deleted"}
 
-    except Exception as e:
+    except Exception:
         db.rollback()
-        raise e
-        
-    finally:
-        db.close()
+        raise
