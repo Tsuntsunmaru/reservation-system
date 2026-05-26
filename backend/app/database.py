0c561bcac662d2b89fec41cc10795dadd09f178a
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-05-26 15:32:57 +0900

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import Session
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -8,3 +9,10 @@ engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
