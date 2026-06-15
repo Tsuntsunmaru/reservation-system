03d9ead840f37b879642c4a3ecce81f5f3cb79f9
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-06-15 12:44:40 +0900

    Update auth.py

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -39,17 +39,20 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 
 @router.post("/login")
 def login(user: LoginUser, db: Session = Depends(get_db)):
-    try:
-        db_user = db.query(User).filter(User.email == user.email).first()
 
-        if not db_user or not verify_password(user.password, db_user.password):
-            raise HTTPException(status_code=401, detail="Unauthorized")
+    for i in range(3):
+        try:
+            db_user = db.query(User).filter(User.email == user.email).first()
+
+            if not db_user or not verify_password(user.password, db_user.password):
+                raise HTTPException(status_code=401, detail="Unauthorized")
         
-        return {"token": create_token({"user_id": db_user.id,"role":db_user.role})}
-        
-    except Exception as e:
-        print("login error:", e)
-        raise HTTPException(status_code=500, detail="login error")
+            return {"access_token": create_token({"user_id": db_user.id,"role":db_user.role})}
+        except Exception as e:
+            print("login retry:",i , e)
+            time.sleep(2)
+            
+    raise HTTPException(status_code=500, detail="login error")
 
 
 @router.put("/users/me")
