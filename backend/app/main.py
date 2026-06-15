e639c5d1a2980ef89c3094d836ab39c64d807dea
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-06-15 12:05:58 +0900

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,36 +25,36 @@ app.include_router(resource.router)
 app.include_router(auth.router)
 app.include_router(admin.router)
 
-try:
-    with engine.connect() as conn:
-        res = conn.execute(text("""
-            SELECT column_name
-            FROM information_schema.columns
-            WHERE table_name='users' AND column_name='username';
-        """))
-        
-        if res.fetchone() is None:
-            conn.execute(text("ALTER TABLE users ADD COLUMN username VARCHAR;"))
-            conn.commit()
+#try:
+   # with engine.connect() as conn:
+    #    res = conn.execute(text("""
+     #       SELECT column_name
+      #      FROM information_schema.columns
+       #     WHERE table_name='users' AND column_name='username';
+        #"""))
+        #
+        #if res.fetchone() is None:
+         #   conn.execute(text("ALTER TABLE users ADD COLUMN username VARCHAR;"))
+          #  conn.commit()
+           # 
+#        res = conn.execute(text("""
+ #           SELECT column_name
+  #          FROM information_schema.columns
+   #         WHERE table_name='bookings' AND column_name='title';
+    #    """))
+     #   
+      #  if res.fetchone() is None:
+       #     conn.execute(text("ALTER TABLE bookings ADD COLUMN title VARCHAR;"))
+        #    conn.commit()
             
-        res = conn.execute(text("""
-            SELECT column_name
-            FROM information_schema.columns
-            WHERE table_name='bookings' AND column_name='title';
-        """))
-        
-        if res.fetchone() is None:
-            conn.execute(text("ALTER TABLE bookings ADD COLUMN title VARCHAR;"))
-            conn.commit()
-            
-        res = conn.execute(text("""
-            SELECT column_name
-            FROM information_schema.columns
-            WHERE table_name='bookings' AND column_name='note';
-        """))
-        
-        if res.fetchone() is None:
-            conn.execute(text("ALTER TABLE bookings ADD COLUMN note TEXT;"))
-            conn.commit()
-except Exception as e:
-    print("DB init error:", e)
+#        res = conn.execute(text("""
+ #           SELECT column_name
+  #          FROM information_schema.columns
+   #         WHERE table_name='bookings' AND column_name='note';
+    #    """))
+     #   
+      #  if res.fetchone() is None:
+       #     conn.execute(text("ALTER TABLE bookings ADD COLUMN note TEXT;"))
+        #    conn.commit()
+#except Exception as e:
+ #   print("DB init error:", e)
