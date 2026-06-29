7907919b9a8bd3bb236ab90a861cd5540a69681d
Tsuntsunmaru <iitsubasa0309@gmail.com>
2026-06-29 10:00:24 +0900

    Update booking.py

--- backend/app/routers/booking.py
+++ backend/app/routers/booking.py
@@ -100,7 +100,7 @@ def delete_booking(
     db: Session = Depends(get_db)
 ):
     try:
-        booking = db.query(Booking).get(Booking_id)
+        booking = db.query(Booking).get(booking_id)
         print("ROLE:", "[" + user.role + "]")
         if not booking:
             raise HTTPException(404, "見つからない")
